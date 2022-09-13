8e87bcfa702782b04cbf080e31878dd5c453998b
Cesar <divino.cesar@microsoft.com>
2022-09-13 00:05:36 +0000

    Updated node.cpp

--- src/hotspot/share/opto/node.cpp
+++ src/hotspot/share/opto/node.cpp
@@ -1476,13 +1476,6 @@ uint Node::hash() const {
   return (sum>>2) + _cnt + Opcode();
 }
 
-uint Node::hash2() const {
-  uint sum = 0;
-  for( uint i=0; i<_cnt; i++ )  // Add in all inputs
-    sum = (sum<<1)-(uintptr_t) (in(i) != NULL ? in(i)->_idx : 0);        // Ignore embedded NULLs
-  return (sum>>2) + _cnt + Opcode();
-}
-
 //------------------------------cmp--------------------------------------------
 // Compare special parts of simple Nodes
 uint Node::cmp( const Node &n ) const {
