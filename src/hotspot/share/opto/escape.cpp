f74a223217044db0354f538b0ed4520ec9668ea3
Cesar <divino.cesar@microsoft.com>
2022-09-21 18:30:15 +0000

    Updated escape.cpp

--- src/hotspot/share/opto/escape.cpp
+++ src/hotspot/share/opto/escape.cpp
@@ -3574,6 +3574,7 @@ bool ConnectionGraph::come_from_allocate(Node* n) const {
       case Op_Parm:
       case Op_GetAndSetN:
       case Op_GetAndSetP:
+      case Op_LoadBarrier:
       case Op_LoadP:
       case Op_LoadPLocked:
       case Op_LoadN:
