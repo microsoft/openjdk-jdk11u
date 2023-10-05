4dcc8cb0f3c0c3c3e323ff1a2699946f39fe3e31
John Tortugo <divcesar@gmail.com>
2023-10-05 16:31:38 -0700

    Diagnostic'able.

--- src/hotspot/share/opto/c2_globals.hpp
+++ src/hotspot/share/opto/c2_globals.hpp
@@ -524,7 +524,7 @@
   develop(bool, TracePostallocExpand, false, "Trace expanding nodes after"  \
           " register allocation.")                                          \
                                                                             \
-  product(bool, ReduceAllocationMerges, true,                               \
+  diagnostic(bool, ReduceAllocationMerges, true,                            \
           "Try to simplify allocation merges before Scalar Replacement")    \
                                                                             \
   develop(bool, TraceReduceAllocationMerges, false,                         \
